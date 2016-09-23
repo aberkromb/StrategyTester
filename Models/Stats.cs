a46f9bad5d5f541c2025f13089089c07c7134540
doriane grey <dorgrey@mail.ru>
2016-09-23 10:22:41 +0300

    add models

---
+++ Models/Stats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public struct Stats
+    {
+        public string Name { get; set; }
+        public int Period { get; set; }
+        public DateTime DateNTime { get; set; }
+        public float Open { get; set; }
+        public float High { get; set; }
+        public float Low { get; set; }
+        public float Close { get; set; }
+    }
+}
---
+++ StatsLoaderFromTxt/TxtReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatsReaderFromTxt
+{
+    public class TxtReader
+    {
+        public void LoadStats(string path)
+        {
+
+        }
+    }
+}
