3537351c293c5a9a75a25d1b9e21337d63a43d07
doriane grey <dorgrey@mail.ru>
2016-09-24 16:24:13 +0300

    add Interface

--- InterfaceStrategyLib/IStrategy.cs
+++ InterfaceStrategyLib/IStrategy.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibsLoader
+namespace InterfaceStrategyLib
 {
-    public class Class1
+    public interface IStrategy
     {
     }
 }
---
+++ LibsLoader/LoadStrategies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using InterfaceStrategyLib;
+using System.Collections.ObjectModel;
+
+namespace LibsLoader
+{
+    public class LoadStrategies
+    {
+        public void Load()
+        {
+            Collection<IStrategy> Modules = new Collection<IStrategy>();
+
+        }
+    }
+}
