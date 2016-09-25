93e62a4b54834d19bc02398b5b0babb24b436dee
doriane grey <dorgrey@mail.ru>
2016-09-25 11:19:31 +0300

    added libs loader

--- LibsLoader/LoadStrategies.cs
+++ LibsLoader/LoadStrategies.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Collections;
 using InterfaceStrategyLib;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace LibsLoader
 {
@@ -15,6 +18,33 @@ namespace LibsLoader
         {
             Collection<IStrategy> Modules = new Collection<IStrategy>();
 
+            string folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"lib");
+
+            string[] files = Directory.GetFiles(folder, "*.dll");
+
+            if (files.Length > 0)
+            {
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        Assembly assembly = Assembly.LoadFrom(file);
+                        foreach (Type type in assembly.GetTypes())
+                        {
+                            Type iface = type.GetInterface("IStrategy");
+                            if (iface != null)
+                            {
+                                IStrategy module = (IStrategy)Activator.CreateInstance(type);
+                                Modules.Add(module);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Ошибка загрузки библиотек\n" + ex.Message);
+                    }
+                }
+            }
         }
     }
 }
